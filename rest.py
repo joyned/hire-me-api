a9ca59f6e8f5d2d685e85cbf3d4f014cd057f7bb
joyned <brunodsbg@gmail.com>
2020-03-18 21:07:01 -0300

    prod variables changed

--- rest.py
+++ rest.py
@@ -82,6 +82,6 @@ def test():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    #port = int(os.environ.get("PORT", 5000))
-    #app.run(host='0.0.0.0', port=port)
+    #app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
