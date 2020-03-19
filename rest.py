66f9b9b8cc7ca35aaa502d540ab600aabd8435f4
joyned <brunodsbg@gmail.com>
2020-03-18 21:03:03 -0300

    login endpoint created

--- rest.py
+++ rest.py
@@ -62,12 +62,26 @@ def get_book(size):
     data = create_all(int(size))
     return jsonify(data)
 
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    return jsonify("success: {}".format(validate_login(data.get('user'), data.get('password'))))
+
+
+def validate_login(user, pwd):
+    if user == 'admin' and pwd == 'admin':
+        return True
+    else:
+        return False
+
+
 @app.route('/api/test', methods=['GET'])
 def test():
     return jsonify('working!')
 
 
 if __name__ == '__main__':
-    #app.run(debug=True)
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
+    #port = int(os.environ.get("PORT", 5000))
+    #app.run(host='0.0.0.0', port=port)
