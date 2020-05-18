a8f40735715763f35407a96fadb0586d69c23b0a
Bruno <brunodsbg@gmail.com>
2020-05-18 11:53:28 -0300

    change error on sql query

--- flaskr/repository/UserRepository.py
+++ flaskr/repository/UserRepository.py
@@ -14,9 +14,11 @@ def insert_new_user(user, pwd):
 
 
 def get_user_by_name(user):
-    sql = "select Usuario.codigo, Usuario.usuario, Usuario.senha, Candidato.Nome, Candidato.Id Id_Candidato" \
-          " from Usuario " \
-          " join Candidato on Candidato.Cod_Usuario = Usuario.Codigo"\
-          " where Usuario = %s"
+    sql = """
+    select Usuario.codigo, Usuario.usuario, Usuario.senha, Candidato.Nome, Candidato.Id Id_Candidato
+    from Usuario 
+    join Candidato on Candidato.Cod_Usuario = Usuario.Codigo
+    where Usuario = %s
+    """
     param = (user)
     return db.execute_query_fetchone(sql, param)
